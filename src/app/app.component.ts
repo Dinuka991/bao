12726eaafff6a89323d160c4f5465c11df0ca742
Darshana Ekanayake <Darshana Ekanayake>
2017-03-13 17:54:19 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <h1 class = red >Darshana git</h1> 
+               <p>App work</p>    `
+                  
+
+})
+export class AppComponent {
+  title = 'Darshana git '
+  
+  
+  ;
+}
